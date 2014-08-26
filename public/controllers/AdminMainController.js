8e0fb7369bb783ea745cce2eae0c1889b1fa1a0f
Misrab <faizullah.misrab@gmail.com>
2014-08-26 16:01:19 +0800

    cyberdyne core2

--- public/controllers/AdminMainController.js
+++ public/controllers/AdminMainController.js
@@ -10,8 +10,38 @@ app.controller('AdminMainController', function($scope) {
 
 	function init() {
     effects();
+    icons();
 	};
 
+
+
+
+  // add icons for various parts of system
+  function icons() {
+    var w = $(window).width();
+    var h = $(window).height();
+    var r = $('#cyberdyne_logo').width()/2;
+
+    var icons = ["misrab.me", "hyperdrive", "captain's log"];
+    var N = icons.length;
+    var padding = 30;
+    for (index in icons) {
+      var x = w/2 + (r + padding)*Math.cos(2*Math.PI*index/N);
+      var y = h/2 + (r + padding)*Math.sin(2*Math.PI*index/N);
+
+      // place div
+      var moon = $("<div class='moon'></div>");
+      $('body').append(moon);
+      moon.css('left', x);
+      moon.css('top', y);
+
+
+    }
+  };
+
+
+
+  // show neural dots
   function effects() {
     setInterval(addDot, 1000);
     //addDot();
