3fb1f80d4b7d86cb01b42fea77e85592a322de58
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2014-09-23 18:02:54 +0800

    going live 1.04

--- models/setup.go
+++ models/setup.go
@@ -46,13 +46,13 @@ func SetupDB() *gorp.DbMap {
     log.Println("Loading initial data...")
 
     // insert the one note for use
-    env := os.Getenv("ENV")
-    // if not staging
-    if env != "staging" && env != "production" {
+    // env := os.Getenv("ENV")
+    // // if not staging
+    // if env != "staging" && env != "production" {
     	n := new(Note)
 	   	err0 := dbmap.Insert(n)
 	   	PanicIf(err0)
-    }
+    // }
    	
 
     return dbmap
